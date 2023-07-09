7a26f819f6204fb251d096370d2843dca2240392
Jefferson Pessoa <jeffersondev@icloud.com>
2023-07-09 11:03:01 -0300

    update x3

--- src/components/Main.Content.jsx
+++ src/components/Main.Content.jsx
@@ -64,7 +64,7 @@ const MainComponent = () => {
   };
 
   return (
-    <div className="container mx-auto px-4 py-8 max-w-lg bg-[#23262c] drop-shadow-[0px_5px_10px_rgba(0,0,0,0.60)]">
+    <div className="container mx-auto px-4 py-8 max-w-lg bg-[#23262c] drop-shadow-[0px_5px_10px_rgba(0,0,0,0.60)] rounded-[20px]">
       <h1 className="text-white text-2xl font-bold mb-4">Adicione sua tarefa:</h1>
       <InputNovaTarefa incluirTarefa={incluirTarefa} />
       {listaTarefas.map((tarefa) => (
